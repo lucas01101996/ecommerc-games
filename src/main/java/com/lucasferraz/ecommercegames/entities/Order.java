e6828dacb153f16f157b975768fedf58aeee96dc
Lucas Ferraz <lucas_nogueira155@hotmail.com>
2021-09-25 14:57:03 -0300

    association of entities

--- src/main/java/com/lucasferraz/ecommercegames/entities/Order.java
+++ src/main/java/com/lucasferraz/ecommercegames/entities/Order.java
@@ -1,12 +1,15 @@
 package com.lucasferraz.ecommercegames.entities;
 
 import java.io.Serializable;
-import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,16 +20,15 @@ public class Order implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private Instant moment;
-	private Double total;
 	
+	@OneToMany(mappedBy = "order")
+	private List<OrderItem> items = new ArrayList<>();
+
 	public Order() {
 	}
 
-	public Order(Long id, Double total, Instant moment) {
+	public Order(Long id) {
 		this.id = id;
-		this.total = total;
-		this.moment = moment;
 	}
 
 	public Long getId() {
@@ -36,21 +38,35 @@ public class Order implements Serializable{
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	public Double getTotal() {
-		return total;
+	
+	public List<OrderItem> getItems() {
+		return items;
 	}
 
-	public void setTotal(Double total) {
-		this.total = total;
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
 
-	public Instant getMoment() {
-		return moment;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
 	}
-
-	public void setMoment(Instant moment) {
-		this.moment = moment;
+	
+	public double getTotal() {
+		double sum = 0.0;
+		for(OrderItem item : items) {
+			sum += item.getSubTotal();
+		}
+		return sum;
 	}
+	
 	
 }
--- src/main/java/com/lucasferraz/ecommercegames/repositories/OrderItemRepository.java
+++ src/main/java/com/lucasferraz/ecommercegames/repositories/OrderItemRepository.java
@@ -3,9 +3,9 @@ package com.lucasferraz.ecommercegames.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.lucasferraz.ecommercegames.entities.Order;
+import com.lucasferraz.ecommercegames.entities.OrderItem;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
 }
